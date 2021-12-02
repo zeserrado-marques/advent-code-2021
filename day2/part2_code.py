4374e6e8b4d439cf8645b22889a0ba49ddf4527b
zeserrado-marques <57963638+zeserrado-marques@users.noreply.github.com>
2021-12-02 22:38:24 +0000

    Robots have taken over the comments. Help!!

--- day2/part2_code.py
+++ day2/part2_code.py
@@ -3,7 +3,32 @@
 
 '''
 
+# beginning file opening protocol of read bits and get an handle of said bits.
 fhand = open('input.txt')
 
 profundo = 0
 horizon_length = 0
+aim = 0
+
+for line in fhand:
+    # parsing the instructions...  bzzt
+    line = line.rstrip()
+    line = line.split()
+
+    # order and distance to travel... bzzt
+    movement = line[0]
+    scale_movement = int(line[1])
+
+    # moving ship.. humm.. submarine in X-axis (horizon_length) and in the other axis which is Y (profundo)... bzzt
+    if movement == 'forward':
+        horizon_length += scale_movement
+        profundo = profundo + (scale_movement * aim)
+
+    # TAKE AIM TO KILL THE ENEMY!!! ups, I meant, take aim to then move submarine (maybe is yellow) in Y... and bzzt also
+    elif movement == 'up':
+        aim -= scale_movement
+    elif movement == 'down':
+        aim += scale_movement
+
+total = horizon_length * profundo
+print(total)
