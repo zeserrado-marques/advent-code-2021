42914988b8448a7fe670c4f16a1576b73b4570fb
zeserrado-marques <57963638+zeserrado-marques@users.noreply.github.com>
2021-12-15 23:32:30 +0000

    we got score boards running. nice

--- day4/part1_code.py
+++ day4/part1_code.py
@@ -6,7 +6,6 @@ Let's create a bingo game
 '''
 import numpy as np
 
-
 #fhand = open('input.txt')
 fhand = open('test.txt')
 
@@ -15,14 +14,15 @@ draw_numbers = None
 bingo_card = []
 
 cards_collection = dict()
+scores_collection = dict()
 card_count = 0
 for line in fhand:
     line = line.rstrip()
-    print(line)
+    #print(line)
 
     # get bingo draw numbers
     if draw_numbers == None:
-        draw_numbers = line
+        draw_numbers = [int(i) for i in line.split(',')]
         continue
     
     # skip empty lines
@@ -36,9 +36,23 @@ for line in fhand:
     if len(bingo_card) == 5:
         current_card = np.asarray_chkfinite(bingo_card)
         cards_collection[f'card_{card_count+1}'] = current_card
+        scores_collection[f'card_{card_count+1}'] = np.zeros(current_card.shape, dtype=int)
 
         bingo_card.clear()
         card_count+=1
 
-for key in cards_collection:
-    print(key, cards_collection[key], sep='\n')
+# find if drawn number is present in bingo cards
+for number in draw_numbers:
+    for card_key in cards_collection:
+        card = cards_collection[card_key]
+        for y in card:
+            for x in y:
+                if number == x:
+                    result = np.where(card == x)
+                    coordinate = (int(result[0]), int(result[1]))
+                    score_card = scores_collection[card_key]
+                    score_card[coordinate[0], coordinate[1]] = 1
+                    print(score_card)
+    break
+
+# falta fazer função em que, cada vez que um score é adicionado, ele vai verificar se há bingo feito ou não
